dac0fd9d1c18ce92cb9b24cffad2d43d1762987a
WallMartin94 <57918288+WallMartin94@users.noreply.github.com>
2021-04-07 16:45:17 +0200

    Add files via upload

---
+++ src/main/java/DBInterface.java
@@ -0,0 +1,7 @@
+public interface DBInterface {
+
+    public boolean connect(String address, String userName, String password);
+    public void disconnect();
+    public boolean isConnected();
+    public String getName(int id);
+}
